7f05d87287c726a16905c4e67883274e74ed9d75
Paullistowel <gawugahpaul.w.e.l@gmail.com>
2026-03-18 13:16:39 +0000

    Initialized tailwind and file structure

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",         // Vite entry
+    "./src/**/*.{ts,tsx}"   // All TypeScript & TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
